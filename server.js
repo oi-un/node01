ae07adc4d479200e45d6d9effd7ceb7ceaa13337
yunji_choi <choiyj@kakao.com>
2024-04-24 23:50:28 +0900

    routing + html 보여주는 방법 배움

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const app = express();
 // app.listen(port번호, ()=>{});
 app.listen(8080, ()=>{
   console.log('http://localhost:8080 에서 서버 실행중');
-})
+});
 
 // 간단한 서버 기능 
-// 메인페이지 접속시에 반갑다는 문장 전송
-app.get('/', (요청, 응답) => {
-  응답.send('반갑다');
-})
+//유저에게 html 파일 보내주려면 sendFile
+app.get('/', (요청, 응답) => {  응답.sendFile(__dirname + '/index.html') });
+app.get('/about', (요청, 응답) => {  응답.sendFile(__dirname + '/about.html') });
+app.get('/news', (요청, 응답)=>{ 응답.send('오늘 비옴') });
+app.get('/shop', (요청, 응답)=>{ 응답.send('쇼핑페이지임') });
