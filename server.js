5871d8eb4fb4a29a2ebd0126a7b45334aabac942
yunji_choi <choiyj@kakao.com>
2024-04-24 23:26:07 +0900

    nodejs 첫날

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express'); // express 라이브러리 사용하겠다는 뜻
+const app = express();
+
+// 서버 띄우는 코드
+// app.listen(port번호, ()=>{});
+app.listen(8080, ()=>{
+  console.log('http://localhost:8080 에서 서버 실행중');
+})
+
+// 간단한 서버 기능
+// 메인페이지 접속시에 반갑다는 문장 전송
+app.get('/', (요청, 응답) => {
+  응답.send('반갑다');
+})
