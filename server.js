b86c49ed65e77238ff7540711e43c4991004ce03
yunji_choi <choiyj@kakao.com>
2024-05-02 12:19:33 +0900

    삭제버튼 누르면 ajax 요청으로 서버로 db삭제 요청함.

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express'); // express 라이브러리 사용하겠다는 뜻
 const app = express();
 
+// mongoDB 연결
+const { MongoClient, ObjectId } = require('mongodb')
+
+const methodOverrid = require('method-override');
+app.use(methodOverrid('_method'));
+
 // css, js, 이미지 등은 public 폴더에 보관.
 // 해당 public 폴더는 서버에 등록해야 안의 파일 사용 가능
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+
 // 요청.body 쓰려면 필요
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-// mongoDB 연결
-const { MongoClient } = require('mongodb')
 
 let db;
 let id = 'cyj15945';
@@ -65,4 +70,70 @@ app.post('/add', async (요청,응답)=>{
     console.log(e);
     응답.status(500).send('서버 에러남');
   }
+})
+
+app.get('/detail/:id', async (요청, 응답)=>{
+  try{
+    let result = await db.collection('post').findOne({ _id :
+    new ObjectId(요청.params.id)});
+
+    if(result != null){
+      console.log(result);
+      응답.render('detail.ejs', {result : result});
+    } else {
+      응답.render('404.ejs');
+    }
+  }catch(err){
+    console.log(err);
+    응답.status(404).render('404.ejs');
+  }
+})
+
+app.get('/edit/:id', async (요청, 응답)=>{
+  try{
+    let result = await db.collection('post').findOne({ _id :
+    new ObjectId(요청.params.id)});
+
+    if(result != null){
+      응답.render('edit.ejs', {result : result});
+    } else{
+      응답.send('url id 잘못 입력함~')
+    }
+  }catch(err){
+    console.log(err);
+  }
+})
+app.post('/edit', async (요청,응답)=>{
+  //유저가 보낸 데이터 요청.body                        
+  let id = 요청.body.id;
+  let title = 요청.body.title;
+  let content = 요청.body.content;
+  try{
+    if(id == null || id == ''){
+      응답.send('id 안왔는데?');
+    } else{
+      if(title == '' || title == null || content == '' || content == null){
+        응답.send('title이랑 content 없는데?');
+      } else{
+        // 글이 너무 길면? : if(content.length){}
+
+        // updateOne({찾을 document}, {수정할 내용})
+        let result = await db.collection('post').updateOne({_id: new ObjectId(id)},
+        {$set: {title: title, content: content}});
+        
+        응답.redirect('/list');
+      }
+    }
+  } catch(e) {
+    // 에러나면 여기거 해주세용
+    console.log(e);
+    응답.status(500).send('서버 에러남');
+  }
+})
+
+app.delete('/delete', async (요청, 응답)=>{
+  console.log(요청.query);
+  let id = 요청.query.docId;
+  await db.collection('post').deleteOne({_id: new ObjectId(id)})
+  응답.send('삭제완료');
 })
